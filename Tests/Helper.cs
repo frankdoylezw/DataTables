ba88740c5b745f60d36fc395002034ce280d1c56
Frank Doyle <frank.doyle@justretirement.com>
2019-05-20 14:09:32 +0100

    Added comments to Helper.cs

--- Tests/Helper.cs
+++ Tests/Helper.cs
@@ -7,6 +7,7 @@ namespace Tests
 {
     internal static class Helper
     {
+        //Compares names of columns. Returns false if column names do not match.
         public static bool CompareColumns(DataTable table1, DataTable table2)
         {
             if (table1.Columns.Cast<DataColumn>().Any(dc => !table2.Columns.Contains(dc.ColumnName))) return false;
@@ -15,6 +16,7 @@ namespace Tests
             }
         }
 
+        //Returns false if tables are null. Returns false if row count does not match. Returns false if column count does not match.
         public static bool CompareRowColumnCount(DataTable table1, DataTable table2)
         {
             {
@@ -28,6 +30,7 @@ namespace Tests
             }
         }
 
+        //Iterates over each table, comparing values (after multiplication factor has been applied). Returns false if the values do not match.
         public static bool CompareRowContent(DataTable table1, DataTable table2)
         {
             if (table1.Rows.Count != table2.Rows.Count)
@@ -39,8 +42,8 @@ namespace Tests
             {
                 var row1 = table1.Rows[i];
                 var row2 = table2.Rows[i];
-                var decimals1 = GetDecimals(row1.ItemArray, (decimal)1.5);
-                var decimals2 = GetDecimals(row2.ItemArray, (decimal)1.0);
+                var decimals1 = GetDecimals(row1.ItemArray, (decimal) 1.5);
+                var decimals2 = GetDecimals(row2.ItemArray, (decimal) 1.0);
                 if (!decimals1.SequenceEqual(decimals2))
                 {
                     return false;
@@ -50,6 +53,7 @@ namespace Tests
             return true;
         }
 
+        //Method returns a list whose members have been multiplied by the 'multiplier' parameter
         private static List<decimal> GetDecimals(object[] array, decimal multiplier)
         {
             var list = new List<decimal>();
